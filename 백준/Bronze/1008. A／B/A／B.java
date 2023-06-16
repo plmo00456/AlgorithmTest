01d4b91a16d61f3d641b1aaf8d945cd35343d40c
plmo00456 <48476536+plmo00456@users.noreply.github.com>
2023-06-16 16:14:34 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			System.out.println(Double.parseDouble(st.nextToken()) / Double.parseDouble(st.nextToken()));
+		}catch(Exception e) {}
+	}
+
+}
