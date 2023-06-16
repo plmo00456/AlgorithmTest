bd13cb3bb65aebb6842144c107dd81fd426e757b
plmo00456 <48476536+plmo00456@users.noreply.github.com>
2023-06-16 17:36:01 +0900

    [Bronze V] Title: A+B - 5, Time: 140 ms, Memory: 15792 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			if(n == 0 && m == 0) break;
+			bw.append((n + m) + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
