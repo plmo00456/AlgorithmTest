759343e58b530575ffbdc9036a45bae10cc86a05
plmo00456 <48476536+plmo00456@users.noreply.github.com>
2023-06-21 23:27:43 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextLong();
+		System.out.println((N-2)*(N-1)*N/6);
+		System.out.println(3);
+	}
+}
