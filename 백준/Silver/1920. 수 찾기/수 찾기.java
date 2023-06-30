cfe39e08cd3c1821f0e053236096c8665a5d0c01
plmo00456 <48476536+plmo00456@users.noreply.github.com>
2023-06-30 16:43:17 +0900

    [Silver IV] Title: 수 찾기, Time: 1340 ms, Memory: 54224 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,23 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
-		String[] A = br.readLine().split(" ");
-		Map<Integer, Integer> map = new HashMap<>();
-		for (int i = 0; i < N; i++)
-			map.put(Integer.parseInt(A[i]), 1);
-
+		String[] strs = br.readLine().split(" ");
+		long[] A = new long[N];
+		for(int i=0; i<N; i++)
+			A[i] = Long.parseLong(strs[i]);
+		Arrays.sort(A);
 		int M = Integer.parseInt(br.readLine());
 		String[] B = br.readLine().split(" ");
-		for (int i = 0; i < M; i++)
-			System.out.println(map.getOrDefault(Integer.parseInt(B[i]), 0));
-
+		for (int i = 0; i < M; i++) {
+			int st = 0;
+			int ed = A.length - 1;
+			long tar = Long.parseLong(B[i]);
+			boolean chk = false;
+			if(st == ed && (A[st] == tar || A[ed] == tar))
+				chk = true;
+			else {				
+				while(st != ed) {
+					int mid = (ed+st)/2;
+					if(A[st] == tar || A[ed] == tar || tar == A[mid]) {
+						chk = true;
+						break;
+					}
+					if(tar < A[mid]) {
+						ed = mid;
+					}else if(tar > A[mid]) {
+						st = mid;
+					}
+					if(ed - st == 1 && (st == mid || ed == mid)) break;
+				}
+			}
+			System.out.println(chk ? 1 : 0);
+		}
 	}
 }
