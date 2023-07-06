506bc870a3e40fe7004cb632f0640efa0a49e9d5
plmo00456 <48476536+plmo00456@users.noreply.github.com>
2023-07-06 16:04:21 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,16 +1,19 @@
 import java.io.*;
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        long sum = 0;
+        BigInteger sum = BigInteger.ZERO;
         for (int i = 0; i < N; i++) {
             int c = str.charAt(i) - 96;
-            sum += c * Math.pow(31, i);
+            BigInteger bi = new BigInteger(String.valueOf(c)).multiply(new BigInteger(String.valueOf((int) Math.pow(31, i))));
+            sum = sum.add(bi);
         }
-        bw.append(sum % 1234567891 + "");
+        bw.append(String.valueOf(sum.mod(BigInteger.valueOf(1234567891L))));
 
         bw.flush();
         bw.close();
