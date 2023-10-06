63cf1d59dddb135b58c5ff4592eb7d56cb30cf4e
WOOJIN PARK <48476536+plmo00456@users.noreply.github.com>
2023-10-06 21:27:13 +0900

    [level 1] Title: K번째수, Time: 0.53 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0; i<commands.length; i++){
+            int a = commands[i][0];
+            int b = commands[i][1];
+            int c = commands[i][2];
+            
+            int[] arr = new int[b-a+1];
+            int tmp = 0;
+            for(int j = a; j<=b; j++){
+                arr[tmp++] = array[j-1];
+            }
+            Arrays.sort(arr);
+            answer[i] = arr[c-1];
+            
+        }
+        return answer;
+    }
+}
